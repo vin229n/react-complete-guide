d42439477ecb7583a0fd05c6137171e8a4a55c8f
vin229n <50581785+vin229n@users.noreply.github.com>
2019-11-18 10:15:38 +0530

    dsadsdsa

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,21 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
+  static getDerivedStateFromProps(props,state) {
+    console.log('[App.js] getDerivedStateFromProps',props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMaount');
+  }
+
   state ={
     persons: [
       {id: 'sdasd',name: 'Vinayak' , age: 21},
@@ -52,7 +67,7 @@ class App extends Component {
   };
 
   render() {
-  
+    console.log('[App.js] render');
     let persons = null;
 
     if(this.state.showPersons) {
